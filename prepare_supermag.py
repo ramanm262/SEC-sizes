4093f399e7013724840a86ca834bf607d9339d3c
rm1277 <rm1277@unh.edu>
2023-11-15 12:13:27 -0500

    Data prep functionality
    Added a script to generate SuperMAG data files, one to generate a single file for all stations in the specified timeframes, and the ability to restrict that timeframe to a specified list of storm times.

---
+++ prepare_supermag.py
@@ -0,0 +1,64 @@
+import glob
+import datetime as dt
+import pandas as pd
+import numpy as np
+import tqdm
+import cdflib
+
+
+def preprocess_supermag(stime, etime, stations, savefile_name, interpolate=True, method="linear", limit=None):
+    """
+    method	: Interpolation method for missing data. Default 'linear'
+    limit	: Limit to the interpolation (in minutes) None for unlimited, 0 for no interpolation.
+              Default 'None'
+    """
+
+
+    for station in stations:
+        mag_files = glob.glob(f"/data/supermag/baseline/{station}/{station}-*-supermag-baseline.csv")
+
+        m = []
+
+        for entry in tqdm.tqdm(sorted(mag_files), desc=f"Preprocessing SuperMag data for {station}"):
+            df = pd.read_csv(entry)
+            df.drop('IAGA', axis=1, inplace=True)
+            df['Date_UTC'] = pd.to_datetime(df['Date_UTC'])
+            df.set_index('Date_UTC', inplace=True, drop=True)
+            df = df.reindex(
+                pd.date_range(start=dt.datetime(df.index[0].year, 1, 1),
+                              end=dt.datetime(df.index[0].year, 12, 31, 23, 59),
+                              freq='1 Min'), copy=True, fill_value=np.NaN)
+            df['Date_UTC'] = df.index
+
+            m.append(df)
+
+        # Concatenate all dataframes for this station into a single dataframe
+        mag_data = pd.concat(m, axis=0, ignore_index=True)
+
+        del m
+
+        mag_data.set_index('Date_UTC', inplace=True, drop=True)
+
+        mag_data = mag_data[stime:etime]
+
+        if interpolate:
+            for param in mag_data.columns:
+                mag_data[param] = mag_data[param].interpolate(method=method, limit=limit)
+
+        # Export to feather format
+        mag_data.reset_index(drop=False).to_feather(f"/data/ramans_files/mag-feather/magData-{station}_"+savefile_name+".feather")
+
+        del mag_data
+
+    print("Done preprocessing!")
+
+
+if __name__ == "__main__":
+    stations_list = ['YKC', 'CBB', 'BLC', 'MEA', 'SIT', 'BOU', 'VIC', 'NEW', 'OTT', 'FRD', 'GIM', 'FCC',
+     'FMC', 'FSP', 'SMI', 'PIN', 'RAL', 'CMO', 'IQA', 'CDC', 'GHC', 'RPB', 'LET',
+     'C06', 'C07', 'C12', 'T16', 'T22', 'T29', 'T32', 'T36', 'T38']
+    syear, eyear = 2008, 2019
+    stime = pd.Timestamp(syear, 1, 1)
+    etime = pd.Timestamp(eyear, 12, 31, 23, 59, 59)
+    preprocess_supermag(stime, etime, stations_list, f"{syear}-{eyear}_NA", interpolate=True, method="linear", limit=None)
+
---
+++ preprocessing.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import datetime as dt
+import tqdm
+
+
+def storm_extract(df, storm_list, B_param, lead, recovery):
+    storms = []
+    stime, etime = [], []  # will store the resulting time stamps here then append them to the storm time df
+    for date in storm_list:
+        stime.append((dt.datetime.strptime(date, '%m-%d-%Y %H:%M'))-pd.Timedelta(hours=lead))
+        etime.append((dt.datetime.strptime(date, '%m-%d-%Y %H:%M'))+pd.Timedelta(hours=recovery))
+    # adds the time stamp lists to the storm_list dataframes
+    storm_list['stime'] = stime
+    storm_list['etime'] = etime
+    for start, end in tqdm.tqdm(zip(storm_list['stime'], storm_list['etime']), total=len(storm_list),
+                                desc="Extracting storms"):
+        storm = df[(df.index >= start) & (df.index <= end)]
+        storm = storm.dropna()
+        if len(storm) != 0:
+            storms.append(storm)			# creates a list of smaller storm time dataframes
+    storms = pd.concat(storms, axis=0, ignore_index=False)
+
+    return storms
+
+
+def load_supermag(stations_list, syear, eyear, B_param="dbn_geo", storm_time_only=True, saving=True,
+                  data_path="/data/ramans_files/mag-feather/"):
+    if saving:
+        mag_data = []
+        for station_num in tqdm.trange(len(stations_list), desc="Loading SuperMAG data"):
+
+            station_name = stations_list[station_num]
+
+            # Load SuperMAG data
+            this_mag_data = pd.read_feather(data_path+f"magData-{station_name}_{syear}-"
+                                            f"{eyear}.feather")
+            this_mag_data = this_mag_data.set_index("Date_UTC", drop=True)
+            this_mag_data = this_mag_data[B_param]
+            this_mag_data.rename(station_name+'_'+B_param, inplace=True)
+            this_mag_data.index = pd.to_datetime(this_mag_data.index)
+            mag_data.append(this_mag_data)
+        mag_data = pd.concat(mag_data, axis=1, ignore_index=False)
+
+        if storm_time_only:
+            storm_list = pd.read_csv("stormList.csv", header=None, names=["dates"])
+            mag_data = storm_extract(mag_data, storm_list["dates"], B_param, lead=12, recovery=24)
+
+        mag_data.to_hdf(f"supermag_processed_{syear}-{eyear}.h5", key="mag_data")
+
+    else:
+        mag_data = pd.read_hdf(f"supermag_processed_{syear}-{eyear}.h5", key="mag_data")
+
+    return mag_data
+
+
+if __name__ == "__main__":
+    stations_list = ['YKC', "CBB"]
+    a=load_supermag(stations_list, 2008, 2019, saving=True)
+    print(a)
